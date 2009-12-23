0b9f7b035222bb2468302b44e77e4e53ff2267b8
Darian Anthony Patrick <dap@darianpatrick.com>
2009-12-23 11:48:38 -0500

    Renamed error sub

--- retrieve.pl
+++ retrieve.pl
@@ -88,22 +88,22 @@ sub wait_and_display_banner {
 
 sub verify_specified_device {
 
-	my $message = sub { say $_[0]; exit(1) };
+	my $error = sub { say $_[0]; exit(1) };
 
 	if ( !$ARGV[0] || $ARGV[0] !~ m/^\/dev/g ) {
-		$message->('Error: No serial device specified.');
+		$error->('Error: No serial device specified.');
 	}
 
 	if ( ! -e $ARGV[0] ) {
-		$message->('Error: Specified device does not exist.');
+		$error->('Error: Specified device does not exist.');
 	}
 
 	if ( ! -r $ARGV[0] ) {
-		$message->('Error: Specified device does not readable.');
+		$error->('Error: Specified device does not readable.');
 	}
 
 	if ( ! -w $ARGV[0] ) {
-		$message->('Error: Specified device does not writable.');
+		$error->('Error: Specified device does not writable.');
 	}
 
 	return $ARGV[0];
