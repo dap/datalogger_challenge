10781feb619ed73641f3d803f6c9fae11450e42c
Darian Anthony Patrick <dap@darianpatrick.com>
2009-12-23 10:46:37 -0500

    Pre-cleanup

--- retrieve.pl
+++ retrieve.pl
@@ -2,6 +2,10 @@
 
 use Modern::Perl;
 use Device::SerialPort;
+use Time::HiRes qw/usleep/;
+use Tie::Cycle;
+
+$| = 1;
 
 my $dev = '/dev/ttyUSB0';
 
@@ -14,38 +18,62 @@ sub main {
 	$port->databits( 8      );
 	$port->stopbits( 1      );
 
-	my ( $num_bytes_read, $bytes_read);
-	my $num_bytes_to_read = 1;
-	my $banner = '';
+	#my ( $num_bytes_read, $banner_bytes_read);
+	#my $num_bytes_to_read = 1;
+	#my $banner = '';
 
-	while ( $banner !~ m/Go$/ ) {
-		($num_bytes_read, $bytes_read)
-			= $port->read($num_bytes_to_read);
+	#while ( $banner !~ m/Go$/ ) {
+	#	($num_bytes_read, $banner_bytes_read)
+	#		= $port->read($num_bytes_to_read);
 
-		$banner .= $bytes_read
-			if $num_bytes_read;
-	}
+	#	$banner .= $banner_bytes_read
+	#		if $num_bytes_read;
+	#}
 
-	say $banner;
+	#say $banner;
 
-	$num_bytes_to_read = 3;
+	my $data_bytes_read = '';
 
-	my $read_command_format = '%02x';
+	#my $read_command_format = '%02x';
+	my $read_command_format = '%x';
 
-	foreach my $address ( 0x00 .. 0xff ) {
-		my $read_command = reverse(
-			sprintf($read_command_format, $address)
-		);
-		$read_command = '>r' . $read_command . "\n";
-		say $read_command;
+	my %bytes_found;
+	my @found_string;
 
-		$port->write( $read_command );
+	tie my $spinner, 'Tie::Cycle', [map {("\b$_")x 5} qw(\ | / -)];
+
+	print 'Working:  ';
+
+	# 16-bit unsigned integer can hold 2^16 = 65536 different
+	# values, it's range being 0 to 65535.
+	foreach my $address ( 0 .. (2**16 - 1) ) {
+		#$address = sprintf($read_command_format, $address);
+		$address = pack('n', $address);
+		#$address = $address;
+		#$address = reverse( $address );
+		#$address = chr $address;
+		#$address = $address;
+
+		$port->write( ">r$address\n" );
+
+		usleep 50000;
 		
-		($num_bytes_read, $bytes_read)
-			= $port->read($num_bytes_to_read);
+		$data_bytes_read = $port->input();
+		print $spinner;
 
-		say $bytes_read;
+		if ( $data_bytes_read ne "r1\0" ) {
+			$data_bytes_read =~ m/r1(.*)/;
+			push @found_string, unpack('a', $1);
+			$bytes_found{$address} = ord $1;
+		}
+
+		#say $num_bytes_read, $read_command, $data_bytes_read;
 	}
+
+	use Data::Dumper qw/Dumper/;
+	#print Dumper(\%bytes_found);
+	say "\nFound ", scalar(keys %bytes_found);
+	say join('', @found_string);
 }
 
 main() if $0 eq __FILE__;
